9c92f2b807518d6413bcddcb0c3457cf99591ebf
Vins <vincentweb984@hotmail.com>
2011-05-06 19:15:20 +0200

    Vector

---
+++ include/Vec.h
@@ -0,0 +1,66 @@
+#ifndef VEC_H
+#define VEC_H
+
+
+class Vec
+{
+    public:
+        double x;
+        double y;
+        double z;
+
+        /** Default constructor */
+        Vec();
+
+        Vec(double const &x, double const &y);
+
+        Vec(double const &x, double const &y, double const &z);
+
+        /** Construct a vector from too vertices */
+        void vect2vect(Vec const &start, Vec const &end);
+
+        Vec copy();
+
+        double norm2();
+
+        double norm();
+
+        void normalize();
+
+        void negate();
+
+        void scalarProd(double const &lambda);
+
+        Vec scalarProduct(double const &lambda);
+
+        /** Calculate the dot product between this and v
+         * \param v the vector with which calculating the dot product
+         * \return The value of the dot product
+         */
+        Vec dotProduct(Vec const &v);
+
+
+        Vec crossProduct(Vec v);
+
+        Vec operator+(Vec const &v);
+
+        Vec operator-(Vec const &v);
+
+        void operator+=(Vec const &v);
+
+        void operator-=(Vec const &v);
+
+        bool operator==(Vec const &v);
+
+        Vec operator=(Vec const &v);
+
+
+
+    protected:
+
+    private:
+        double norm; //!< norm of the vector
+        double norm2; //!< norm^2 of the vector
+};
+
+#endif // VEC_H
---
+++ src/Vec.cpp
@@ -0,0 +1,90 @@
+#include "Vec.h"
+
+Vec::Vec()
+{
+    x     = 0;
+    y     = 0;
+    z     = 0;
+    norm  = -1;
+    norm2 = -1;
+
+}
+
+Vec::Vec(double const &x, double const &y, double const &z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vec::Vec(double const &x, double const &y)
+{
+    Vec(x,y,0);
+}
+
+Vec::vect2vect(Vec const &start, Vec const &end)
+{
+    x = end.x - start.x;
+    y = end.y - start.y;
+    z = end.z - start.z;
+}
+
+Vec::copy()
+{
+    return new Vec(x,y,z);
+}
+
+Vec::norm2()
+{
+    if (norm2 == -1)
+    {
+        norm2 = x*x + y*y + z*z;
+    }
+    return norm2;
+}
+
+Vec::norm()
+{
+    if (norm == -1)
+    {
+        norm = sqrt(norm2);
+    }
+    return norm;
+}
+
+Vec::normalize()
+{
+    x /= this->norm();
+    y /= this->norm();
+    z /= this->norm();
+}
+
+Vec::negate()
+{
+    x = -x;
+    y = -y;
+    z = -z;
+}
+
+Vec::scalarProd(double const &lambda)
+{
+    x *= lambda;
+    y *= lambda;
+    z *= lambda;
+}
+
+Vec::scalarProduct(double const &lambda)
+{
+    return new Vec(x * lambda, y * lambda, z * lambda);
+}
+
+Vec::dotProduct(Vec const &v)
+{
+    return x*v.x + y*v.y + z*v.z;
+}
+
+Vec::crossProduct(Vec v)
+{
+    return new Vec(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
+}
+
