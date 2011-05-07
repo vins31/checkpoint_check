5eabc727ee54a9bee3d31f1be8f2bb168ba3f934
Vins <vincentweb984@hotmail.com>
2011-05-07 20:16:59 +0200

    bug fixs

---
+++ include/CheckPoints.h
@@ -0,0 +1,25 @@
+#ifndef CHECKPOINTS_H
+#define CHECKPOINTS_H
+
+#include <vector>
+
+#include "Poly.h"
+
+class CheckPoints
+{
+    public:
+        std::vector<Poly*> polys;
+
+        /** Default constructor */
+        CheckPoints();
+
+        void add(Poly* p);
+
+    protected:
+    private:
+
+};
+
+std::ostream& operator<< (std::ostream &os, CheckPoints const &cp);
+
+#endif // CHECKPOINTS_H
---
+++ src/CheckPoints.cpp
@@ -0,0 +1,22 @@
+#include "CheckPoints.h"
+
+CheckPoints::CheckPoints()
+{
+    //ctor
+}
+
+void CheckPoints::add(Poly* p)
+{
+    polys.push_back(p);
+}
+
+std::ostream& operator<< (std::ostream &os, CheckPoints const &cp)
+{
+    os << "CheckPoints:" << std::endl;
+    for ( size_t i = 0, size = cp.polys.size(); i < size; ++i )
+    {
+        os << "  [" << i << "] ";
+        std::cout << *(cp.polys[i]) << std::endl;
+    }
+    return os;
+}
